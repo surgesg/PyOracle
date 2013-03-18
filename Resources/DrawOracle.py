1289beed197e93688b37f254e63c064a52f1e50f
Greg Surges <surgesg@gmail.com>
2013-03-18 15:20:15 -0700

    simplified IR.py and DrawOracle.py
    --HG--
    branch : simplified_oracle

--- Resources/DrawOracle.py
+++ Resources/DrawOracle.py
@@ -23,47 +23,47 @@ def start_draw(_oracle, size=(900*4, 400*4)):
 
 def draw(current_state):
     # handle to Draw object - PIL
+    N_states = len(oracle['sfx'])
     draw = ImageDraw.Draw(image)
-    for i, state in enumerate(oracle):
+    for i in range(N_states):
         # draw circle for each state
-        x_pos = (float(i) / len(oracle) * width) + 0.5 * 1.0 / len(oracle) * width
+        x_pos = (float(i) / N_states * width) + 0.5 * 1.0 / N_states * width
         # iterate over forward transitions
-        for tran in state.transition:
+        for tran in oracle['trn'][i]:
             # if forward transition to next state
-            if tran.pointer.number == i + 1:
+            if tran == i + 1:
                 # draw forward transitions
-                next_x = (float(i + 1) / len(oracle) * width) + 0.5 * 1.0 / len(oracle) * width
-                current_x = x_pos + (0.25 / len(oracle) * width)
+                next_x = (float(i + 1) / N_states * width) + 0.5 * 1.0 / N_states * width
+                current_x = x_pos + (0.25 / N_states * width)
                 draw.line((current_x, height/2, next_x, height/2), width=1,fill='white')
             else:
-                if tran.pointer.lrs >= lrs_threshold:
+                if oracle['lrs'][tran] >= lrs_threshold:
                     # forward transition to another state
                     current_x = x_pos
-                    next_x = (float(tran.pointer.number) / len(oracle) * width) + (0.5 / len(oracle) * width)
-                    arc_height = (height / 2) + (tran.pointer.number - state.number) * 0.125
+                    next_x = (float(tran) / N_states * width) + (0.5 / N_states * width)
+                    arc_height = (height / 2) + (tran - i) * 0.125
                     draw.arc((int(current_x), int(height/2 - arc_height/2),
                         int(next_x), int(height/2 + arc_height / 2)), 180, 0,
                         fill='White')
-        if state.suffix:
-            if state.suffix.number != 0 and state.suffix.lrs >= lrs_threshold:
-                current_x = x_pos
-                next_x = (float(state.suffix.number) / len(oracle) * width) + (0.5 / len(oracle) * width)
-                # draw arc
-                arc_height = (height / 2) - (state.suffix.number - state.number) * 0.125
-                draw.arc((int(next_x), 
-                          int(height/2 - arc_height/2), 
-                          int(current_x), 
-                          int(height/2 + arc_height/2)),
-                        0,
-                        180,
-                        fill='White')
+        if oracle['sfx'][i] is not None and oracle['sfx'][i] != 0 and oracle['lrs'][oracle['sfx'][i]] >= lrs_threshold:
+            current_x = x_pos
+            next_x = (float(oracle['sfx'][i]) / N_states * width) + (0.5 / N_states * width)
+            # draw arc
+            arc_height = (height / 2) - (oracle['sfx'][i] - i) * 0.125
+            draw.arc((int(next_x), 
+                      int(height/2 - arc_height/2), 
+                      int(current_x), 
+                      int(height/2 + arc_height/2)),
+                    0,
+                    180,
+                    fill='White')
     # draw circles on top
-    for i, state in enumerate(oracle):
+    for i in range(N_states):
         color = 'white'
         if i == current_state:
             color = 'red'
-        x_pos = (float(i) / len(oracle) * width) + 0.5 * 1.0 / len(oracle) * width
-        circle_width = (0.5 / len(oracle) * width) / 2 
+        x_pos = (float(i) / N_states * width) + 0.5 * 1.0 / N_states * width
+        circle_width = (0.5 / N_states * width) / 2 
         draw.ellipse([x_pos - circle_width, height/2 - circle_width,
             x_pos+circle_width, height/2 + circle_width], outline='white', fill='white')
     image.resize((900, 400), (Image.BILINEAR))
--- Resources/PyOracle/PyOracle.py
+++ Resources/PyOracle/PyOracle.py
@@ -53,10 +53,6 @@ def get_distance(event1, event2, weights = None):
             distance += data
     return distance
 
-'''
-def add_initial_state(states):
-    states.append(State.State(0))
-'''
 def add_initial_state(oracle):
     oracle['sfx'].append(None)
     oracle['rsfx'].append([])
@@ -78,7 +74,6 @@ def add_state(oracle, new_data, threshold = 0, weights = None):
     oracle['trn'][i - 1].append(i)
 
     k = oracle['sfx'][i - 1] 
-    print n_states, i, k
     pi_1 = i - 1
 
     # iteratively backtrack suffixes from state i-1
@@ -120,56 +115,6 @@ def add_state(oracle, new_data, threshold = 0, weights = None):
             while oracle['sfx'][pi_2] != oracle['sfx'][pi_1]:
                 pi_2 = oracle['sfx'][pi_2]
             oracle['lrs'][-1] = min(oracle['lrs'][pi_1], oracle['lrs'][pi_2]) + 1
-
-'''
-def add_state(states, new_data, threshold = 0, weights = None):
-    # create a new state numbered i
-    new_state = State.State(len(states))
-    states.append(new_state) 
-    # assign a new transition from state i - 1 to state i
-    states[-2].add_transition(new_data, states[-1]) 
-    k = states[-2].suffix
-    pi_1 = states[-2] # from shlomo's code
-    # iteratively backtrack suffixes from state i - 1
-    # adding shlomo's suggested improvement - gs 06.04.2012
-    while k != 0:
-        # trying this with < threshold
-        dvec = [get_distance(new_data, s.data, weights) < threshold for s in k.transition]
-        # if there is not a transition from the suffix
-        if True not in dvec:
-            k.add_transition(new_data, states[-1])
-            pi_1 = k # from shlomo's code
-            k = k.suffix
-        else:
-            break
-    # if backtrack ended before state 0:
-    if k == None or k == 0:
-        states[-1].add_suffix(states[0])
-    else:
-        # filter out all above distance thresh - 06.04.2012
-        filtered_transitions = filter(lambda x: get_distance(x.data, new_data, weights) <= threshold, k.transition)
-        # sort the possible suffixes by lrs
-        sorted_list = sorted(filtered_transitions, key = lambda x: x.pointer.lrs)
-        for t in sorted_list:
-            if get_distance(t.data, new_data, weights) <= threshold: 
-                S_i = t
-                states[-1].add_suffix(S_i.pointer)
-                # add reverse suffix
-                S_i.pointer.add_reverse_suffix(states[-1])
-                break
-    # LRS algorithm from shlomo's AOS.m
-    ss = states[-1].suffix
-    if ss == 0 or ss.number == 0 or ss.number == 1:
-        states[-1].lrs = 0
-    else:
-        pi_2 = states[ss.number - 1]
-        if pi_2 == pi_1.suffix:
-            states[-1].lrs = pi_1.lrs + 1
-        else:
-            while pi_2.suffix != pi_1.suffix:
-                pi_2 = pi_2.suffix
-            states[-1].lrs = min(pi_1.lrs, pi_2.lrs) + 1
-'''
 
 def build_oracle(input_data, threshold, feature = None):
     # features should be determined by the analysis code
